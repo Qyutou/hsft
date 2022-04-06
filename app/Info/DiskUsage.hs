470ef79ce2d87b97beebc874407c967045e66c7e
Qyutou <aseledkov04@gmail.com>
2022-04-06 13:30:33 +0700

    Create the tool to fetch used and available space on disk

---
+++ app/Info/DiskUsage.hs
@@ -0,0 +1,58 @@
+{-|
+Module      : Info.DiskUsage
+Description : Module which gets current disk usage
+Copyright   : (c) Alexey Seledkov, 2022
+License     : MIT
+Maintainer  : qyutou@gmail.com
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Info.DiskUsage 
+    ( fetchDiskUsage
+    , fetchAvailSpace
+    ) where
+
+import qualified Data.Text as T
+
+import Info.Cmd (fetchCmd)
+import System.Directory (doesDirectoryExist)
+
+-- | Get current disk usage in format 'used/total' of certain dir
+fetchDiskUsage :: FilePath  -- ^ Path to directory
+               -> IO T.Text -- ^ Disk usage
+fetchDiskUsage p = do
+    isPathCorrent <- doesDirectoryExist p
+    if isPathCorrent
+        then do
+            dfResult <- fetchCmd "df" ["-h", T.pack p] 
+            let dfWords = reverse . T.words $ dfResult
+            if length dfWords > 5
+                then do
+                    print dfWords
+                    return $ T.concat [dfWords !! 3, "/", dfWords !! 4]
+                else
+                    return "not found"
+        else
+            return "not found"
+
+-- | Get current available space on disk
+fetchAvailSpace :: FilePath  -- ^ Path to directory
+                -> IO T.Text -- ^ Disk usage
+fetchAvailSpace p = do
+    isPathCorrent <- doesDirectoryExist p
+    if isPathCorrent
+        then do
+            dfResult <- fetchCmd "df" ["-h", T.pack p] 
+            let dfWords = reverse . T.words $ dfResult
+            if length dfWords > 3
+                then do
+                    print dfWords
+                    return $ dfWords !! 2
+                else
+                    return "not found"
+        else
+            return "not found"
+
+
+    
